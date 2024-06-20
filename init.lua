7bb10d37105e9bf866b20ff3010d85f2e204da8f
Eugene Dashkevich <jabs.box@gmail.com>
2024-06-20 09:52:54 +0200

    cleanup linters

--- init.lua
+++ init.lua
@@ -668,7 +668,7 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        javascript = { { 'eslint_d', 'eslint', 'prettierd', 'prettier' } },
+        javascript = { 'prettierd', 'eslint_d' },
       },
     },
   },
