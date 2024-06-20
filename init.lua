fd084cc1a991bf4aba24f58861fc4cfb42e9bc36
Eugene Dashkevich <jabs.box@gmail.com>
2024-06-20 09:52:54 +0200

    cleanup linters

--- init.lua
+++ init.lua
@@ -715,7 +715,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        javascript = { 'eslint_d', 'eslint', 'prettierd', 'prettier' },
+        javascript = { 'prettierd', 'eslint_d' },
       },
     },
   },
