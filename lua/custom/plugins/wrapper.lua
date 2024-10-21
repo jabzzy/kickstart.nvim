5b8850b62752ccb7d920bf94d0d51eb452e43f48
Eugene Dashkevich <jabs.box@gmail.com>
2024-10-21 11:33:44 +0200

    adjust md softener

--- lua/custom/plugins/wrapper.lua
+++ lua/custom/plugins/wrapper.lua
@@ -1,6 +1,8 @@
 return {
   'andrewferrier/wrapping.nvim',
   config = function()
-    require('wrapping').setup()
+    require('wrapping').setup {
+      softener = { default = 1.0, markdown = 1.5 },
+    }
   end,
 }
